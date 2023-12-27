51698abb7455b1d89e151e5b165ba116df99285a
Nick Thompson <49480739+NickThompson42@users.noreply.github.com>
2023-12-27 15:14:56 -0600

    Create linux_baseline_install.sh
    create baseline shell script

---
+++ linux_baseline_install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Ensure the script is run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Update and Upgrade the System
+echo "Updating and upgrading the system..."
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Install necessary base packages
+echo "Installing base packages..."
+sudo apt-get install -y git emacs nano vim curl wget software-properties-common
+
+# Install necessary packages for Docker
+echo "Installing necessary packages for Docker..."
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Add Docker’s official GPG key
+echo "Adding Docker's official GPG key..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the Docker repository
+echo "Adding the Docker repository..."
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update package database with Docker packages from the newly added repo
+echo "Updating package database..."
+sudo apt-get update
+
+# Install Docker
+echo "Installing Docker..."
+sudo apt-get install -y docker-ce
+
+# Check if the docker group exists, create if it doesn't, and add the current user to it
+echo "Adding current user to the docker group..."
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+
+# Enable and start Docker
+echo "Enabling and starting Docker..."
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Install NVIDIA Container Toolkit for Docker to access GPUs
+echo "Installing NVIDIA Container Toolkit..."
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+  && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+  && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+echo "Installation complete. Please log out and log back in to apply the changes."
