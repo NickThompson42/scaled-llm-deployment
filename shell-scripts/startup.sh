93108812f82b626e75ed15df653a7566ed5aede6
Nick Thompson <49480739+NickThompson42@users.noreply.github.com>
2023-12-29 10:32:34 -0600

    moved nvidia and cuda install to end
    moved nvidia and cuda install to the end of the startup.sh script to make sure ever other dependency was installed before reboot

--- shell-scripts/startup.sh
+++ shell-scripts/startup.sh
@@ -42,6 +42,59 @@ echo "Installing Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Clone the necessary repositories
+echo "Cloning the repositories..."
+git clone https://github.com/Royce-Geospatial-Consultants/h2ogpt_rg.git $USER_HOME/h2ogpt_rg
+git clone https://github.com/NickThompson42/scaled-llm-deployment.git $USER_HOME/scaled-llm-deployment
+
+# Set up bash functions for the user
+echo "Setting up bash functions for user..."
+
+# Create the ~/.bashrc_functions file if it doesn't exist
+if [ ! -f "$USER_HOME/.bashrc_functions" ]; then
+    touch "$USER_HOME/.bashrc_functions"
+    chown "$SUDO_USER":"$SUDO_USER" "$USER_HOME/.bashrc_functions"
+fi
+
+# Append the docker_startup function to the ~/.bashrc_functions
+cat << EOF >> "$USER_HOME/.bashrc_functions"
+function docker_startup(){
+    # make run_docker_compose executable and run it
+    sudo chmod +x $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
+    sudo $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
+}
+
+function baseline(){
+   # source the needed functions
+   source $USER_HOME/.bashrc_functions
+   # change dir to h2ogpt_rg
+   cd $USER_HOME/h2ogpt_rg
+   ls
+   echo "Verify the files exist in h2ogpt_rg"
+   sleep 10
+   echo "Immediately after this message, use the 'docker_startup' command."
+}
+EOF
+
+# Ensure the .bashrc sources the functions file
+if ! grep -q ".bashrc_functions" "$USER_HOME/.bashrc"; then
+    cat << EOF >> "$USER_HOME/.bashrc"
+# Custom functions for enhanced bash experience
+source \$HOME/.bashrc_functions
+EOF
+    chown "$SUDO_USER":"$SUDO_USER" "$USER_HOME/.bashrc"
+fi
+
+echo -e " "
+echo -e ">> Installation complete."
+echo -e " "
+echo -e ">>> After reboot, use command 'baseline'."
+echo -e " "
+echo -e ">>>> After reboot, run 'docker_startup' to initialize the docker container for the LLM."
+echo -e " "
+echo -e ">>>>> Please log out and log back in to apply the changes."
+
+
 # Install NVIDIA Container Toolkit for Docker to access GPUs
 echo "Installing NVIDIA Container Toolkit..."
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
@@ -98,58 +151,6 @@ else
     echo "Unsupported GPU type: $GPU_TYPE"
     exit 1
 fi
-
-# Clone the necessary repositories
-echo "Cloning the repositories..."
-git clone https://github.com/Royce-Geospatial-Consultants/h2ogpt_rg.git $USER_HOME/h2ogpt_rg
-git clone https://github.com/NickThompson42/scaled-llm-deployment.git $USER_HOME/scaled-llm-deployment
-
-# Set up bash functions for the user
-echo "Setting up bash functions for user..."
-
-# Create the ~/.bashrc_functions file if it doesn't exist
-if [ ! -f "$USER_HOME/.bashrc_functions" ]; then
-    touch "$USER_HOME/.bashrc_functions"
-    chown "$SUDO_USER":"$SUDO_USER" "$USER_HOME/.bashrc_functions"
-fi
-
-# Append the docker_startup function to the ~/.bashrc_functions
-cat << EOF >> "$USER_HOME/.bashrc_functions"
-function docker_startup(){
-    # make run_docker_compose executable and run it
-    sudo chmod +x $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
-    sudo $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
-}
-
-function baseline(){
-   # source the needed functions
-   source $USER_HOME/.bashrc_functions
-   # change dir to h2ogpt_rg
-   cd $USER_HOME/h2ogpt_rg
-   ls
-   echo "Verify the files exist in h2ogpt_rg"
-   sleep 10
-   echo "Immediately after this message, use the 'docker_startup' command."
-}
-EOF
-
-# Ensure the .bashrc sources the functions file
-if ! grep -q ".bashrc_functions" "$USER_HOME/.bashrc"; then
-    cat << EOF >> "$USER_HOME/.bashrc"
-# Custom functions for enhanced bash experience
-source \$HOME/.bashrc_functions
-EOF
-    chown "$SUDO_USER":"$SUDO_USER" "$USER_HOME/.bashrc"
-fi
-
-echo -e " "
-echo -e ">> Installation complete."
-echo -e " "
-echo -e ">>> After reboot, use command 'baseline'."
-echo -e " "
-echo -e ">>>> After reboot, run 'docker_startup' to initialize the docker container for the LLM."
-echo -e " "
-echo -e ">>>>> Please log out and log back in to apply the changes."
 
 # Offer to reboot the system
 read -p "Do you want to reboot now? (y/n) " -n 1 -r
