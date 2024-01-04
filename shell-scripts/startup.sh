b9cb02b5e780c21064dc0f88e52f3d404c502866
Nick Thompson <49480739+NickThompson42@users.noreply.github.com>
2024-01-04 16:11:42 -0600

    working towards making it more idempotent
    added checks for repo install and bashrc amendments
    next,
    - work on package updates, docker and docker compose installation
    - configuration and environment variables
    - nvidia and cuda toolkit checks
    - error handling

--- shell-scripts/startup.sh
+++ shell-scripts/startup.sh
@@ -25,19 +25,46 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=nonin
 
 # echo "Starting system update and package installation..."
 
-# # Update package list and upgrade the system
-# sudo apt-get update && sudo apt-get upgrade -y
-
 # Clean up unnecessary packages
 sudo apt autoremove -y
 
 # Install necessary base packages
 sudo apt-get install -y git emacs nano vim curl wget software-properties-common pandoc apt-transport-https ca-certificates
 
-# Clone the necessary repositories
-echo "Cloning the repositories..."
-git clone https://github.com/Royce-Geospatial-Consultants/h2ogpt_rg.git $USER_HOME/h2ogpt_rg
-git clone https://github.com/NickThompson42/scaled-llm-deployment.git $USER_HOME/scaled-llm-deployment
+####################################
+#### LAST WORKING FRESH INSTALL ###
+####################################
+
+# # Clone the necessary repositories
+# echo "Cloning the repositories..."
+# git clone https://github.com/Royce-Geospatial-Consultants/h2ogpt_rg.git $USER_HOME/h2ogpt_rg
+# git clone https://github.com/NickThompson42/scaled-llm-deployment.git $USER_HOME/scaled-llm-deployment
+
+############
+### END ####
+############
+
+############
+### NEW ####
+############
+
+# Check if repositories already cloned
+if [ ! -d "$USER_HOME/h2ogpt_rg" ]; then
+    git clone https://github.com/Royce-Geospatial-Consultants/h2ogpt_rg.git $USER_HOME/h2ogpt_rg
+else
+    echo "h2ogpt_rg already cloned."
+fi
+
+if [ ! -d "$USER_HOME/scaled-llm-deployment" ]; then
+    git clone https://github.com/NickThompson42/scaled-llm-deployment.git $USER_HOME/scaled-llm-deployment
+else
+    echo "scaled-llm-deployment already cloned."
+fi
+
+############
+### END ####
+############
+
 
 # Set up bash functions for the user
 echo "Setting up bash functions for user..."
@@ -48,25 +75,58 @@ if [ ! -f "$USER_HOME/.bashrc_functions" ]; then
     chown "$SUDO_USER":"$SUDO_USER" "$USER_HOME/.bashrc_functions"
 fi
 
-# Append the docker_startup function to the ~/.bashrc_functions
-cat << EOF >> "$USER_HOME/.bashrc_functions"
-function docker_startup(){
-    # make run_docker_compose executable and run it
-    sudo chmod +x $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
-    sudo $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
+####################################
+#### LAST WORKING FRESH INSTALL ###
+####################################
+
+# # Append the docker_startup function to the ~/.bashrc_functions
+# cat << EOF >> "$USER_HOME/.bashrc_functions"
+# function docker_startup(){
+#     # make run_docker_compose executable and run it
+#     sudo chmod +x $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
+#     sudo $USER_HOME/scaled-llm-deployment/shell-scripts/run_docker_compose.sh
+# }
+
+# function baseline(){
+#    # source the needed functions
+#    source $USER_HOME/.bashrc_functions
+#    # change dir to h2ogpt_rg
+#    cd $USER_HOME/h2ogpt_rg
+#    ls
+#    echo "Verify the files exist in h2ogpt_rg"
+#    sleep 10
+#    echo "Immediately after this message, use the 'docker_startup' command."
+# }
+# EOF
+
+############
+### END ####
+############
+
+##########################
+#### NEW BASH STARTUP ####
+##########################
+
+# Change in .bashrc to prevent docker_startup from running on every shell start
+cat << EOF >> "$USER_HOME/.bashrc"
+# Custom command to start docker containers if not already running
+function ensure_docker_containers_running(){
+    CONTAINER_COUNT=\$(docker ps | wc -l)
+    # If no containers are running, then start them
+    if [ "\$CONTAINER_COUNT" -eq 1 ]; then
+        docker_startup
+    else
+        echo "Docker containers already running."
+    fi
 }
 
-function baseline(){
-   # source the needed functions
-   source $USER_HOME/.bashrc_functions
-   # change dir to h2ogpt_rg
-   cd $USER_HOME/h2ogpt_rg
-   ls
-   echo "Verify the files exist in h2ogpt_rg"
-   sleep 10
-   echo "Immediately after this message, use the 'docker_startup' command."
-}
+# Change directory and conditionally run docker_startup at the start of every session
+cd $USER_HOME/h2ogpt_rg && docker pull gcr.io/vorvan/h2oai/h2ogpt-runtime:0.1.0 && ensure_docker_containers_running
 EOF
+
+############
+### END ####
+############
 
 # Ensure the .bashrc sources the functions file
 if ! grep -q ".bashrc_functions" "$USER_HOME/.bashrc"; then
@@ -79,9 +139,9 @@ fi
 
 cat << EOF >> "$USER_HOME/.bashrc"
 # Change directory and run docker_startup at the start of every session
-cd $USER_HOME/h2ogpt_rg && docker_startup
+cd $USER_HOME/h2ogpt_rg && docker pull gcr.io/vorvan/h2oai/h2ogpt-runtime:0.1.0
+ && docker_startup
 EOF
-
 
 # Install Docker
 echo "Installing Docker..."
@@ -95,47 +155,6 @@ sudo usermod -aG docker $SUDO_USER
 echo "Installing Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-######################################
-## DOES NOT WORK BUT GOOD REFERENCE ##
-######################################
-
-# # Install NVIDIA Container Toolkit for Docker to access GPUs
-# echo "Installing NVIDIA Container Toolkit..."
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-# curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-# curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-# sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-# sudo systemctl restart docker
-
-# # Install the NVIDIA driver 440 for the server
-# echo "Installing NVIDIA driver 440..."
-# sudo apt-get install -y nvidia-driver-440-server
-
-# # Install CUDA Toolkit 12.3
-# echo "Installing CUDA Toolkit 12.3..."
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-# sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-# wget https://developer.download.nvidia.com/compute/cuda/12.3.0/local_installers/cuda-repo-ubuntu2004-12-3-local_12.3.0-545.23.06-1_amd64.deb
-# sudo dpkg -i cuda-repo-ubuntu2004-12-3-local_12.3.0-545.23.06-1_amd64.deb
-# sudo cp /var/cuda-repo-ubuntu2004-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
-# sudo apt-get update
-# sudo apt-get -y install cuda-toolkit-12-2
-
-# # Install nvtop for monitoring
-# sud apt install nvtop -y
-
-# Remove the old NVIDIA driver installation line
-# echo "Installing NVIDIA driver 440..."
-# sudo apt-get install -y nvidia-driver-440-server
-
-###################################
-## END, NEXT SECTION REPLACES IT ##
-###################################
-
-######################
-## REPLACEMENT CODE ##
-######################
 
 # Install NVIDIA Container Toolkit for Docker to access GPUs
 echo "Installing NVIDIA Container Toolkit..."
@@ -166,10 +185,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y install cuda-toolkit-12-3
 # Install nvtop for monitoring
 echo "Installing nvtop for monitoring..."
 sudo apt install nvtop -y
-
-#########
-## END ##
-#########
 
 # Add necessary environment variables to .bashrc
 echo "Configuring environment variables..."
